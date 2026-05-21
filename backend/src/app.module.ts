08d30feeaa0285db44e125b4b50e143a43a3bd23
Mateo Serebrinsky <mserebrinsky@mail.austral.edu.ar>
2026-05-21 16:09:03 -0300

    fix: remove repeated line

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,7 +4,6 @@ import { AuthModule } from './modules/auth/auth.module';
 import { APP_GUARD } from '@nestjs/core';
 import { JwtAuthGuard } from './modules/public/guards/jwt-auth.guard';
 import { UsersModule } from './modules/users/users.module';
-import { EdgarModule } from './modules/edgar/edgar.module';
 import { PricesModule } from './modules/prices/prices.module';
 import { EdgarModule } from './modules/edgar/edgar.module';
 import { PortfolioModule } from './modules/portfolio/portfolio.module';
